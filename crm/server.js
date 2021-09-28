9f3233b9f7517564c0d14af57a8bb79c0250dbea
Elodie Sayavong <sayavong.pro@gmail.com>
2021-09-28 12:13:58 +0200

    [crm] add debug middleware

--- crm/server.js
+++ crm/server.js
@@ -4,10 +4,13 @@ const dotenv = require("dotenv");
 dotenv.config({
     path: "./config.env",
 });
+// Import middlewares
+const debug = require("./middleware/debug");
 // Import routers
 const register = require("./router/register");
 
 app.use(express.json());
+app.use(debug);
 app.use("/register", register);
 
 // Starting server
